510b855655e798b7760e8e432d93f4b62ddbd015
nastia <nikolaevaav55@gmail.com>
2023-02-20 17:54:35 +0300

    change elements by tag

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
-const bgc = window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
+const bgc = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
 
-function toggleText(button_id) 
+function toggleText(button_id)
 {
    var el = document.getElementById(button_id);
-   if (el.firstChild.data == "Версия для слабовидящих") 
+   if (el.firstChild.data == "Версия для слабовидящих")
    {
-       var bgc = window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
-       var bgc = String(bgc)
-       console.log(bgc)
        var color = "white"
        changeFontSize(el.firstChild.data, color)
        el.firstChild.data = "Обычная версия";
-
    }
-   else 
+   else
    {
     changeFontSize(el.firstChild.data, bgc)
      el.firstChild.data = "Версия для слабовидящих";
@@ -24,28 +20,22 @@ function toggleText(button_id)
 
 function changeFontSize(type, color){
 
-    // IDs to select
-    let ids = ["#h", '#p'];
-    
-    
+    let tags = ["h1","h2", "h3", "p", "a", "h4"];
 
-    ids.forEach(id => {
-        // Get element
-        let el = document.querySelector(id);
+     tags.forEach(tag => {
+         var tags = document.querySelectorAll(tag);
+           for(var i=0; i<tags.length; i++){
+             let fontSize = window.getComputedStyle(tags[i], null).getPropertyValue("font-size")
+             fontSize = parseFloat(fontSize);
+              if(type === "Версия для слабовидящих"){
+                 tags[i].style.fontSize = (fontSize * 2) + "px";
+                 tags[i].style.color = "green";
+                 document.body.style.background = color
 
-        // Get font size
-        let fontSize = window.getComputedStyle(el, null).getPropertyValue("font-size")
+             }else{
+                 tags[i].style.fontSize = (fontSize / 2) + "px";
+                 document.body.style.background = color
+                 }}
 
-        // Convert to float
-        fontSize = parseFloat(fontSize);
-
-        // Increase / Decrease font-size
-        if(type === "Версия для слабовидящих"){
-            el.style.fontSize = (fontSize * 2) + "px";
-            document.body.style.background = "white"
-        }else{
-            el.style.fontSize = (fontSize / 2) + "px";
-            document.body.style.background = bgc
-        }
-    });
-}
+       });
+       }
