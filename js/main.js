ec60c51016a676371010e5f6516ec7b1dab5ef32
nastia <nikolaevaav55@gmail.com>
2023-02-22 12:06:22 +0300

    new_page added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 const bgc = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
 
+
+function CheckCookies()
+{
+    if($.cookie('site_version') == 'Версия для слабовидящих')
+    var el = document.getElementById("siteVersion");
+    el.firstChild.data = "Обычная версия";
+    var color = "white"
+    changeFontSize("Версия для слабовидящих", color)
+
+}
+
+
+
 function toggleText(button_id)
 {
    var el = document.getElementById(button_id);
    if (el.firstChild.data == "Версия для слабовидящих")
-   {
+   {   
+       $.cookie('site_version', "Версия для слабовидящих", { path: '/' });
        var color = "white"
        changeFontSize(el.firstChild.data, color)
        el.firstChild.data = "Обычная версия";
    }
    else
    {
+    $.removeCookie('site_version');
     changeFontSize(el.firstChild.data, bgc)
-     el.firstChild.data = "Версия для слабовидящих";
+    el.firstChild.data = "Версия для слабовидящих";
    }
 }
+
+
+CheckCookies()
 
 
 
@@ -29,7 +47,6 @@ function changeFontSize(type, color){
              fontSize = parseFloat(fontSize);
               if(type === "Версия для слабовидящих"){
                  tags[i].style.fontSize = (fontSize * 2) + "px";
-                 tags[i].style.color = "green";
                  document.body.style.background = color
 
              }else{
